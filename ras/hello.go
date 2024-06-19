a3d7edd2abf4bdb268939e6246a1a18c7c8540b0
Yash Chauhan <yashc22@iitk.ac.in>
2024-06-19 15:58:17 +0530

    email template fixes

--- ras/hello.go
+++ ras/hello.go
@@ -22,7 +22,7 @@ func PlaceHolderController(c *gin.Context) {
 func MailController(mail_channel chan mail.Mail) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		mail_channel <- mail.GenerateMail("yashc22@iitk.ac.in", "Test Mail", "Hello World!")
-		mail_channel <- mail.GenerateMails([]string{"utkarshm21.ac.in", "krishnansh21@iitk.ac.in"}, "Test Mail to multiple ppl", "Hello Worlds!")
+		mail_channel <- mail.GenerateMails([]string{"yashlm1017@gmail.com", "bmerchant22@iitk.ac.in"}, "Test Mail to multiple ppl", "Hello Worlds!")
 		c.JSON(http.StatusOK, gin.H{"message": "Mail sent"})
 	}
 }
